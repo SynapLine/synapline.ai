dd1bdccf9ae64e870a196ecaea6dcbcf4af3f887
Malik Aliyev <malik.aliyev.94@gmail.com>
2022-04-05 11:25:53 +0400

    Career

--- base.js
+++ base.js
@@ -8,22 +8,36 @@ new fullpage('#fullpage', {
   // afterSlideLoad: function (section, origin, destination, direction) { }
   // navigationTooltips: ['Who we are', 'Our team', 'Lorem', 'Ipsum'],
   // showActiveTooltip: true,
-  afterSlideLoad: function (section, origin, destination, direction) {
-    console.log({
-      section: section,
-      origin: origin,
-      destination: destination,
-      direction: direction
-    });
+  responsiveSlides: true,
+  responsiveWidth: 900,
+
+  // onLeave: function (origin, destination, direction) {
+  //   console.log('1')
+  // },
+  afterLoad: function (origin, destination, direction) {
+    if (destination.index < 2) {
+      var videoElement = document.getElementById('video');
+      videoElement.play()
+    }
   },
-  onSlideLeave: function (section, origin, destination, direction) {
-    console.log({
-      section: section,
-      origin: origin,
-      destination: destination,
-      direction: direction
-    });
-  }
+  // afterRender: function () {
+  //   console.log('3')
+  // },
+  // afterResize: function (width, height) {
+  //   console.log('4')
+  // },
+  // afterReBuild: function () {
+  //   console.log('5')
+  // },
+  // afterResponsive: function (isResponsive) {
+  //   console.log('6')
+  // },
+  // afterSlideLoad: function (section, origin, destination, direction) {
+  //   console.log('7')
+  // },
+  // onSlideLeave: function (section, origin, destination, direction) {
+  //   console.log('8')
+  // }
 });
 
 //methods
